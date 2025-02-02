17306d9ac3362c7ac7427f08bc6bb68e6701aa2b
mi-co22 <rgn.mico.main@gmail.com>
2025-02-02 21:00:10 +0900

    🚧 PC　ざっくりスライダーまで実装

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,6 +2,43 @@ $(function () {
     //-----------------------------------------------------
     // スライダー
     //-----------------------------------------------------
+    // スライダー機能
+    function initSwiper() {
+        var swiper;
+
+        function initializeSwiper() {
+            swiper = new Swiper('.swiper', {
+                spaceBetween: 10,
+                slidesPerView: 1.5,
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+            });
+        }
+
+        function destroySwiper() {
+            if (swiper) {
+                swiper.destroy();
+                swiper = undefined;
+            }
+        }
+
+        function handleResize() {
+            var w = $(window).width();
+            if (w < 768) {
+                if (!swiper) {
+                    initializeSwiper();
+                }
+            } else {
+                destroySwiper();
+            }
+        }
+
+        handleResize();
+        $(window).on('resize', handleResize);
+    }
+    initSwiper();
 });
 
 
