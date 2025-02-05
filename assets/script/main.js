43ce589db45ca64894358c81f55695777ae6c561
mi-co22 <rgn.mico.main@gmail.com>
2025-02-05 15:43:36 +0900

    🚀 最終調整＆初稿提出

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,45 +2,45 @@ $(function () {
     //-----------------------------------------------------
     // スライダー
     //-----------------------------------------------------
-    function initSwiper() {
-        let swiper;
+    let swiper;
+    const breakpoint = 768;
 
-        function initializeSwiper() {
-            swiper = new Swiper('.swiper', {
-                spaceBetween: 15,
-                loop: true,
-                slidesPerView: 1.245,
-                centeredSlides: true,
-                autoHeight: true,
-                pagination: {
-                    el: ".swiper-pagination",
-                    clickable: true,
-                },
-            });
-        }
-
-        function destroySwiper() {
-            if (swiper) {
-                swiper.destroy();
-                swiper = undefined;
-            }
-        }
-
-        function handleResize() {
-            var w = $(window).width();
-            if (w < 768) {
-                if (!swiper) {
-                    initializeSwiper();
-                }
-            } else {
-                destroySwiper();
-            }
-        }
-
-        handleResize();
-        $(window).on('resize', handleResize);
+    function initializeSwiper() {
+        swiper = new Swiper('.swiper', {
+            spaceBetween: 15,
+            loop: true,
+            slidesPerView: 1.245,
+            centeredSlides: true,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
     }
-    initSwiper();
+
+    function destroySwiper() {
+        if (swiper) {
+            swiper.destroy(true, true);
+            swiper = null;
+        }
+    }
+
+    function handleResize() {
+        let w = window.innerWidth;
+        if (w < breakpoint) {
+            if (!swiper) {
+                initializeSwiper();
+            }
+        } else {
+            destroySwiper();
+        }
+    }
+
+    // 初回実行
+    handleResize();
+
+    // リサイズ時に再評価
+    window.addEventListener('resize', handleResize);
 });
 
 
