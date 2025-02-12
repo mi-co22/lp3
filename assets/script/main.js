e1327413be40fcb54a91df34a4c82b7e88819cf3
mi-co22 <rgn.mico.main@gmail.com>
2025-02-12 13:32:36 +0900

    🔥 コメント閉じタグ削除＆日本語変更。swiper仕様シンプル化。フォント読み込みを一行にまとめ

--- assets/script/main.js
+++ assets/script/main.js
@@ -4,43 +4,25 @@ $(function () {
     //-----------------------------------------------------
     let swiper;
     const breakpoint = 768;
-
-    function initializeSwiper() {
-        swiper = new Swiper('.swiper', {
-            spaceBetween: 15,
-            loop: true,
-            slidesPerView: 1.245,
-            centeredSlides: true,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            },
-        });
-    }
-
-    function destroySwiper() {
-        if (swiper) {
-            swiper.destroy(true, true);
-            swiper = null;
-        }
-    }
-
-    function handleResize() {
-        let w = window.innerWidth;
-        if (w < breakpoint) {
-            if (!swiper) {
-                initializeSwiper();
+    swiper = new Swiper('.swiper', {
+        spaceBetween: 15,
+        loop: true,
+        slidesPerView: 1.245,
+        centeredSlides: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 14,
+                watchOverflow: true,
+                allowTouchMove: false,
+                centeredSlides: false
             }
-        } else {
-            destroySwiper();
         }
-    }
-
-    // 初回実行
-    handleResize();
-
-    // リサイズ時に再評価
-    window.addEventListener('resize', handleResize);
+    });
 });
 
 
